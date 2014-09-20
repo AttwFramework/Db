ecbaa040cb161835f260d3e42e7009aa030b816c
GabrielJMJ <gamjj74@hotmail.com>
2014-09-20 11:22:44 -0300

    Fixes bugs

--- SQL/MySQL/Statement/Delete.php
+++ SQL/MySQL/Statement/Delete.php
@@ -9,8 +9,7 @@
 
 namespace Attw\DB\SQL\MySQL\Statement;
 
-use Attw\DB\SQL\AbstractStatement;
-use Attw\DB\SQL\MySQL\Clause\Where;
+use Attw\DB\SQL\MySQL\Statement\AbstractStatementWithWhere;
 use Attw\DB\SQL\MySQL\Operator\Equal;
 use Attw\DB\SQL\MySQL\Operator\AndOperator;
 use Attw\DB\SQL\MySQL\Clause\From;
@@ -18,7 +17,7 @@ use Attw\DB\SQL\MySQL\Clause\From;
 /**
  * MySQL SQL Delete statement
 */
-class Delete extends AbstractStatement
+class Delete extends AbstractStatementWithWhere
 {
     /**
      * Table to delete some registries
@@ -28,39 +27,15 @@ class Delete extends AbstractStatement
     private $table;
 
     /**
-     * Where clause to indicate some registry
-     *
-     * @var \Attw\DB\SQL\MySQL\Clause\Where
-    */
-    private $where;
-
-    /**
      * Contruct a delete statement
      *
      * @param string $table
      * @param array|string $where can be array or a string
-     * @param string $operator Operator to separate $where (Default: AND)
-     * @throws \InvliadArgumentException case $table is not a string
-     * @throws \InvliadArgumentException case $operator is diferent of AND and OR
     */
     public function __construct($table, $where)
     {
-        if (!is_string($table)) {
-            throw new \InvalidArgumentException(sprintf('%s::%s: the table must be a string', get_class($this), __METHOD__));
-        }
-
         $this->table = new From($table);
-
-        if (is_array($where)) {
-            $equals = array();
-            foreach ($where as $column => $value) {
-                $equals[] = new Equal($column, $value);
-            }
-
-            $this->where = new Where(new AndOperator($equals));
-        } else {
-            $this->where = new Where($where);
-        }
+        $this->constructWhere($where);
     }
 
     /**
